b5d7d1e0e89fcb621b870d4ae6d20f6b8d90e9ae
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-08-22 19:07:59 +0500

    #fixed surname and birthdate null problem

--- models/user.go
+++ models/user.go
@@ -16,8 +16,8 @@ type User struct {
 type Client struct {
 	Id        int     `json:"id" db:"id"`
 	Name      *string `json:"name" form:"name"`
-	Surname   *string `json:"surname" form:"surname" default:""`
-	Birthdate *string `json:"birthdate" form:"birthdate" default:"2006-01-02"`
+	Surname   *string `json:"surname" form:"surname"`
+	Birthdate *string `json:"birthdate" form:"birthdate"`
 	Gender    *string `json:"gender" form:"gender"`
 	Avatar    *string `json:"avatar" form:"avatar"`
 	Phone     *string `json:"phone" db:"phone"`
